a3c84908c10bfdd5243ece917fba7e01fff98243
NK8998 <ne.kioko16@gmail.com>
2026-02-02 11:38:54 +0300

    framebuffers start

--- advanced/FrameBuffers_CubeMaps_AdvancedData_AdvancedGLSL/src/source/main.cpp
+++ advanced/FrameBuffers_CubeMaps_AdvancedData_AdvancedGLSL/src/source/main.cpp
@@ -17,7 +17,6 @@
 #include <model.h>
 #include <textures.h>
 
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
@@ -173,6 +172,37 @@ int main() {
         1.0f, -0.5f,  0.0f,  1.0f,  1.0f,
         1.0f,  0.5f,  0.0f,  1.0f,  0.0f
     };
+
+    ////frame buffer
+    //unsigned int fbo;
+    //glGenFramebuffers(1, &fbo);
+    //glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+
+    //unsigned int texture;
+    //glGenTextures(1, &texture);
+    //glBindTexture(GL_TEXTURE_2D, texture);
+
+    ////color attachment
+    //glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    //glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture, 0);
+
+    ////depth and stencil attachment
+    //glTexImage2D(
+    //    GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, SCR_WIDTH, SCR_HEIGHT, 0,
+    //    GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, NULL
+    //);
+    //glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, texture, 0);
+
+    ////renderbuffer object attachments
+    //unsigned int rbo;
+    //glGenRenderbuffers(1, &rbo);
+
+
+    //if (glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE);
 
     //cube
     unsigned int cubeVAO, cubeVBO;
